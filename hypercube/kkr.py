3bd45b9e859b4028fb34b8211ebf8ef96b70f84c
Hypatia Meraviglia <hbarry@cub.uca.edu>
2023-05-31 12:45:13 -0500

    kkr.py pulls from literature data

--- hypercube/kkr.py
+++ hypercube/kkr.py
@@ -7,18 +7,37 @@ import scipy.fftpack as ft
 #rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 #rc('text',usetex=True)
 
-g = 0.1
-w0 = 5.0
-def chii(w):
-    return(g**2)/((w - w0)**2 + g**2)
+dataset = "../lit/he_2022/he2022_imag_refrac.txt"
 
-w = np.linspace(0, 10, 800)
+#Sets I as function of chi which is a function of frequency
 
+#g = 0.1
+#w0 = 5.0
+#def chii(w):
+#    return(g**2)/((w - w0)**2 + g**2)
+#k = chii(w)
+#n = ft.hilbert(chii(w))
+
+#w = np.linspace(0, 10, 800)
+
+#Reads in array of I from lit
+f = open(dataset, "r")
+lines = f.readlines()[1:]
+w = []
+k = []
+for i in lines:
+    w.append(float(i.split(' ')[0]))
+    k.append(float(i.split(' ')[1]))
+f.close()
+
+n = ft.hilbert(k)
+
+#Plot n and k by freq
 fig, ax = plt.subplots()
-ax.plot(w, ft.hilbert(chii(w)), label = '${\\rm Re}[\chi(\omega)]$')
-ax.plot(w, chii(w), 'r-', label = '${\\rm Im}[\chi(\omega)]$')
-ax.set_xlabel('$\omega$" fontsize = 18')
-ax.set_ylabel("${\\rm Im}[\chi(\omega)]$", fontsize = 18)
+ax.plot(w, n, label = '${\\rm Re}[w]$')
+ax.plot(w, k, 'r-', label = '${\\rm Im}[w]$')
+ax.set_xlabel("$\omega$", fontsize = 18)
+ax.set_ylabel("${\\rm Im}[w]$", fontsize = 18)
 ax.set_title("Real RI calculated from Imaginary RI via KKR")
 ax.legend()
 plt.show()
