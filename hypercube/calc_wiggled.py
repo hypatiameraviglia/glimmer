f96b958af83fcf66abc433f3f7e96121f1141a71
Hypatia Meraviglia <hbarry@cub.uca.edu>
2023-06-19 10:32:22 -0500

    Created class ri to store ri data

--- hypercube/calc_wiggled.py
+++ hypercube/calc_wiggled.py
@@ -1,4 +1,17 @@
 #calc_wiggled.py
 
 import numpy as np
-from glimmer import ri
+from glimmer import ri_wiggler
+
+num_wiggled_indices = 10
+
+#TODO: read in # of wavels
+#Wiggle indices num_wiggled_indices times
+def wiggle_indices_n_times(indices):
+    wiggled_indices = [[0 for i in range(num_wiggled_indices)] for j in range(wavel)]
+    for i in range(num_wiggled_indices):
+        #Make a copy of original indices list
+        indices_copy = ri_wiggler.copy_ri(indices)
+        #Wiggle each index at each wavelength point in list
+        wiggled_indices = ri_wiggler.wiggle_indices(indices_copy)
+        
---
+++ hypercube/ri.py
@@ -0,0 +1,11 @@
+class ri:
+    def __init__(self, dataset, wavel, n, k, dn, dk)
+        self.wavel = wavel
+        self.dataset = dataset
+        self.n = real
+        self.k = k
+        self.dn = dn
+        self. dk = dk
+    def __str__(self, dataset, wavel, n, k, dn, dk)
+        print("{self.dataset} is a group of ref. ind. of length ", len(n))
+
