3bd19bab7d9adee212ea1a4bee8950cb580077b3
Hypatia Meraviglia <hbarry@cub.uca.edu>
2023-06-26 13:14:13 -0500

    Added maxs and mins to class structure

--- hypercube/ri.py
+++ hypercube/ri.py
@@ -3,10 +3,14 @@ class ri:
     def __init__(self, dataset, wavel, n, k, dn, dk)
         self.wavel = wavel
         self.dataset = dataset
-        self.n = real
+        self.n = n
         self.k = k
         self.dn = dn
-        self. dk = dk
+        self.dk = dk
+        self.nmax = n + dn
+        self.nmin = n - dn
+        self.kmax = k + dk
+        self.kmin = k - dk
     def __str__(self, dataset, wavel, n, k, dn, dk)
         print("{self.dataset} is a group of ref. ind. of length ", len(n))
 
