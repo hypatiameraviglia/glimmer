b3abb0e055edba61b9054e84fb149b6d9da5d9c8
Hypatia Meraviglia <hbarry@cub.uca.edu>
2023-06-26 13:41:47 -0500

    Added dataset-dependent errors to read_in_lit.py

--- hypercube/read_in_lit.py
+++ hypercube/read_in_lit.py
@@ -7,6 +7,8 @@ for filename in os.listdir(directory):
             f = open(filename, "r")
 
 #refrac data by wavel
+ri.dataset = f.readlines()[0]
+ri.temp = f.readlines()[1]
 lines = f.readlines()[3:]
 
 #TODO: what happens when two studies overlap? (same wavel, same T)
@@ -14,6 +16,47 @@ for i in lines:
     ri.wavel.append(float(i.split('   ')[1])) #check splitting chars
     ri.n.append(float(i.split('   ')[2]))
     ri.k.append(float(i.split('   ')[3]))
-
+    
 f.close()
+
+#Populate error based on study; see paper
+if filename == "warren1984":
+
+if filename == "mastrapa2008_Ic":
+
+if filename == "toon1994":
+    for i in ri.k:
+        ri.dk[i] = ri.k[i]*0.20
+
+if filename == "warrenbrandt2008":
+
+if filename == "bertie1969":
+    for i in ri.k:
+        ri.dk[i] = ri.k[i]*0.05
+
+if filename == "clapp1995":
+
+if filename == "he2022":
+    for i in ri.k:
+        ri.dk[i] = ri.k[i]*0.01
+if filename == "perovichandgovoni1991":
+
+if filename == "leger1983":
+
+if filename == "mukaiandkraetschmer1986":
+
+if filename == "hudgins1993":
+    for i in ri.k:
+        ri.dk[i] = ri.k[i]*0.05
+
+if filename == "mastrapa2008_Ia":
+
+if filename == "browellandanderson1975":
+    for i in ri.k:
+        ri.dk[i] = ri.k[i]*0.10
+
+if filename == "rousch1997":
+
+if filename == "kofman2019":
+
 
