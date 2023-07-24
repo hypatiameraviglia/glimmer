620b741baf48da270ab422a434aba26c569bdc39
Hypatia Meraviglia <hbarry@cub.uca.edu>
2023-07-24 12:47:01 -0700

    Wrote test for read_in_lit

--- hypercube/read_in_lit.py
+++ hypercube/read_in_lit.py
@@ -1,62 +1,72 @@
 #Open .txt files from literature studies and populate ri objects with data
 import ri from ri.py
 
-directory = "~/scattering/lit" #TODO: does this reach into subdir?
-for filename in os.listdir(directory):
+    directory = "~/scattering/lit"
+def read_data(directory):
+    #TODO: does this reach into subdir?
+    for filename in os.listdir(directory):
         if filename.endswith('.txt'):   #only the refrac files
             f = open(filename, "r")
 
-#refrac data by wavel
-ri.dataset = f.readlines()[0]
-ri.temp = f.readlines()[1]
-lines = f.readlines()[3:]
+    #refrac data by wavel
+    ri.dataset = f.readlines()[0]
+    ri.temp = f.readlines()[1]
+    lines = f.readlines()[3:]
 
-#TODO: what happens when two studies overlap? (same wavel, same T)
-for i in lines:
-    ri.wavel.append(float(i.split('   ')[1])) #check splitting chars
-    ri.n.append(float(i.split('   ')[2]))
-    ri.k.append(float(i.split('   ')[3]))
+    #TODO: what happens when two studies overlap? (same wavel, same T)
+    for i in lines:
+        ri.wavel.append(float(i.split('   ')[1])) #check splitting chars
+        ri.n.append(float(i.split('   ')[2]))
+        ri.k.append(float(i.split('   ')[3]))
     
-f.close()
+            f.close()
+    
+    return ri
 
-#Populate error based on study; see paper
-if filename == "warren1984":
+def get_error():
+#Populate error based on study; see respective papers
+    if ri.dataset == "test2023":
+    #Test file for use by test_read_in_lit.py
+        for i in ri.wavel:
+            ri.dk[i] = ri.k[i]*0.20
 
-if filename == "mastrapa2008_Ic":
+    if ri.dataset == "warren1984":
 
-if filename == "toon1994":
-    for i in ri.k:
-        ri.dk[i] = ri.k[i]*0.20
+    if ri.dataset == "mastrapa2008_Ic":
 
-if filename == "warrenbrandt2008":
+    if ri.dataset == "toon1994":
+        for i in ri.k:
+            ri.dk[i] = ri.k[i]*0.20
 
-if filename == "bertie1969":
-    for i in ri.k:
-        ri.dk[i] = ri.k[i]*0.05
+    if ri.dataset == "warrenbrandt2008":
 
-if filename == "clapp1995":
+    if ri.dataset == "bertie1969":
+        for i in ri.k:
+            ri.dk[i] = ri.k[i]*0.05
 
-if filename == "he2022":
-    for i in ri.k:
-        ri.dk[i] = ri.k[i]*0.01
-if filename == "perovichandgovoni1991":
+    if ri.dataset == "clapp1995":
 
-if filename == "leger1983":
+    if ri.dataset == "he2022":
+        for i in ri.k:
+            ri.dk[i] = ri.k[i]*0.01
+    if ri.dataset == "perovichandgovoni1991":
 
-if filename == "mukaiandkraetschmer1986":
+    if ri.dataset == "leger1983":
 
-if filename == "hudgins1993":
-    for i in ri.k:
-        ri.dk[i] = ri.k[i]*0.05
+    if ri.dataset == "mukaiandkraetschmer1986":
 
-if filename == "mastrapa2008_Ia":
+    if ri.dataset == "hudgins1993":
+        for i in ri.k:
+            ri.dk[i] = ri.k[i]*0.05
 
-if filename == "browellandanderson1975":
-    for i in ri.k:
-        ri.dk[i] = ri.k[i]*0.10
+    if ri.dataset == "mastrapa2008_Ia":
 
-if filename == "rousch1997":
+    if ri.dataset == "browellandanderson1975":
+        for i in ri.k:
+            ri.dk[i] = ri.k[i]*0.10
 
-if filename == "kofman2019":
+    if ri.dataset == "rousch1997":
+
+    if ri.dataset == "kofman2019":
 
 
---
+++ tests/test_read_in_lit.py
@@ -0,0 +1,66 @@
+#Tests read_in_lit.py, which sort info from .txt files in literature directory 
+#into objects of the class ri, defined in ri.py
+
+import unittest
+
+from .context import glimmer
+from glimmer import read_in_lit
+from glimmer import ri
+
+import os
+
+class TestReadInLit(unittest.TestCase):
+
+    def setUp(self):
+        directory = "~/scattering/lit/test"
+
+# Tests of read_data
+
+    def test_dataset_is_string(self):
+        dataset = read_in_lit.read_data(directory).dataset
+        self.assertEqual(type(dataset), type(""))
+
+    def test_ks_in_array(self):
+        k = read_in_lit.read_data(directory).k
+        self.assertEqual(type(k), type([]))
+
+    def test_ks_are_floats(self):
+        k = read_in_lit.read_data(directory).k
+        for i in k:
+            self.assertEqual(type(i), type(float(0.1)))
+
+    def test_wavels_in_array(self):
+        wavel = read_in_lit.read_data(directory).wavel
+        self.assertEqual(type(wavel), type([]))
+
+    def test_wavels_are_floats(self):
+        wavel = read_in_lit.read_data(directory).wavel
+        for i in wavel:
+            self.assertEqual(type(i), type(float(0.1)))
+
+    def test_ks_same_len_as_wavels(self):
+        k = read_in_lit.read_data(directory).k
+        wavel = read_in_lit.read_data(directory).wavel
+        self.assertEqual(len(k), len(wavel))
+
+#Tests of get_error
+
+    def test_dk_in_array(self):
+        dk = read_in_lit.get_error(read_in_lit.read_data(directory)).dk
+        self.assertEqual(type(dk), type([]))
+
+    def test_dk_same_len_as_wavels(self):
+        dk = read_in_lit.get_error(read_in_lit.read_data(directory)).dk
+        wavel = read_in_lit.read_data(directory).wavel
+        self.assertEqual(len(dk), len(wavel))
+
+    def test_dks_are_floats(self):
+        dk = read_in_lit.get_error(read_in_lit.read_data(directory)).dk
+        for i in dk:
+            self.assertEqual(type(i), type(float(0.1)))
+
+    def test_error_value(self):
+        k = read_in_lit.read_data(directory).k
+        dk = read_in_lit.get_error().dk
+        for i in dk:
+            self.assertEqual(i/k, 0.20)
