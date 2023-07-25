22cdaedb5f92a3def7440cf95e8de886b2ab33d2
Hypatia Meraviglia <hbarry@cub.uca.edu>
2023-07-25 09:13:44 -0700

    Added error axes to interpolate.py

--- hypercube/interpolate.py
+++ hypercube/interpolate.py
@@ -18,21 +18,31 @@ wavel_max = 30
 wavel_step = 0.05
 
 def interpolate(ri):
-    #Cubic spline interpolation
+    #Establish temperature and wavelength as 2d mesh
     temp_axis = np.linspace(min(ri.temp), max(ri.temp))
     wavel_axis = np.linspace(min(ri.wavel), max(ri.wavel))
     temp_axis, wavel_axis = np.meshgrid(temp_axis, wavel_axis)
 
+    #Interpolate n
     n_interp = LinearNDInterpolator(list(zip(ri.temp, ri.wavel)), ri.n)
     n_axis = n_interp(temp_axis, wavel_axis)
 
+    #Interpolate k
     k_interp = LinearNDInterpolator(list(zip(ri.temp, ri.wavel)), ri.k)
     k_axis = k_interp(temp_axis, wavel_axis)
 
-return temp_axis, wavel_axis, n_axis, k_axis
+    #Interpolate dn
+    dn_interp = LinearNDInterpolator(list(zip(ri.temp, ri.wavel)), ri.dn)
+    dn_axis = dn_interp(temp_axis, wavel_axis)
 
-def plot_interpolation(ri, temp_axis, wavel_axis, n_axis, k_axis):
-    #Plotting interpolated n
+    #Interpolate dk
+    dk_interp = LinearNDInterpolator(list(zip(ri.temp, ri.wavel)), ri.dk)
+    dk_axis = dk_interp(temp_axis, wavel_axis)
+
+return ri, temp_axis, wavel_axis, n_axis, k_axis, dn_axis, dk_axis
+
+def plot_interpolation(ri, temp_axis, wavel_axis, n_axis, k_axis, dn_axis, dk_axis):
+    #Plot interp'd n
     plt.pcolormesh(temp_axis, wavel_axis, n_axis, shading='auto')
     plt.plot(ri.temp, ri.wavel, "ok", label="original data")
     plt.legend()
@@ -40,10 +50,45 @@ def plot_interpolation(ri, temp_axis, wavel_axis, n_axis, k_axis):
     plt.axis("equal")
     plt.savefig("n_interpolated_temp_wavel.png")
 
-    #Plotting interpolated k
+    #Plot interp'd k
     plt.pcolormesh(temp_axis, wavel_axis, k_axis, shading='auto')
     plt.plot(ri.temp, ri.wavel, "ok", label="original data")
     plt.legend()
     plt.colorbar()
     plt.axis("equal")
     plt.savefig("k_interpolated_temp_wavel.png")
+
+    #Plot interp'd dn
+    plt.pcolormesh(temp_axis, wavel_axis, dn_axis, shading='auto')
+    plt.plot(ri.temp, ri.wavel, "ok", label="original data")
+    plt.legend()
+    plt.colorbar()
+    plt.axis("equal")
+    plt.savefig("dn_interpolated_temp_wavel.png")
+
+    #Plot interp'd dk
+    plt.pcolormesh(temp_axis, wavel_axis, dk_axis, shading='auto')
+    plt.plot(ri.temp, ri.wavel, "ok", label="original data")
+    plt.legend()
+    plt.colorbar()
+    plt.axis("equal")
+    plt.savefig("dk_interpolated_temp_wavel.png")
+
+    #Plot all interp'd data together
+    plt.pcolormesh(temp_axis, wavel_axis, n_axis, shading='auto')
+    plt.plot(ri.temp, ri.wavel, "ok", label="original data")
+        
+    plt.pcolormesh(temp_axis, wavel_axis, k_axis, shading='auto')
+    plt.plot(ri.temp, ri.wavel, "ob", label="original data")
+    
+    plt.pcolormesh(temp_axis, wavel_axis, dn_axis, shading='auto')
+    plt.plot(ri.temp, ri.wavel, "og", label="original data")
+    
+    plt.pcolormesh(temp_axis, wavel_axis, dk_axis, shading='auto')
+    plt.plot(ri.temp, ri.wavel, "oc", label="original data")
+
+    plt.legend()
+    plt.colorbar()
+    plt.axis("equal")
+    plt.savefig("all_interpolated_temp_wavel.png")
+
