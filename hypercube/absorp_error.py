Resolved incorrect minimum dk from absorp_error.py. Run equation for absorp error calculation past colleagues

hypercube/absorp_error.py
@@ -9,8 +9,14 @@ import scipy.constants
 def calc_error_from_dalpha(ri, perc_dalpha):
     #See Meraviglia et al. (2023) appendix A and Leger et al. (1983) for 
     # details on calculation of error from absorption coefficient
+    #Convert wavelength in units of microns to units of cm
+    microns_to_cm = float(1E-4)
     for i in range(len(ri.wavel)):
-        ri.dk[i] = ri.k[i]*(perc_dalpha*ri.wavel[i])/(4*np.pi*scipy.constants.c)
+        ri.dk[i] = float(np.absolute(ri.k[i])*(perc_dalpha*ri.wavel[i]*microns_to_cm)/(4*np.pi))
+    print("dks: ", ri.dk)
+    print("perc_dalpha: ", perc_dalpha)
+    print("ks: ", ri.k)
+    print("wavels: ", ri.wavel)
     return ri.dk
 
 def perovich(ri, dalpha_array):